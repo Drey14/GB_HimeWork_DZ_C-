fcd7219ca374c44c46e520080fa4ddfa772d98eb
”Andrey <Soda14@mail.ru>
2023-06-05 00:02:17 +0300

    DownLoad DZ_4_C#

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Урок 4. Функции
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+*/
+
+void DegreeOfNumber(int numberNatural, int degre){
+int totalNum=numberNatural;
+for (int i = 1; i < degre; i++)
+{
+    totalNum= totalNum*numberNatural;
+}
+Console.WriteLine($" Число {numberNatural} в степени {degre} равно: {totalNum}");
+}
+
+Console.WriteLine("Введите число: ");
+int numberNatural = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int degre = Convert.ToInt32(Console.ReadLine());
+DegreeOfNumber(numberNatural,degre);
+
---
+++ Task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Условия задачи не корректны, а именно тесты ниже !!!! Задача согласно условию написана ниже
+
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+void arrayEightNumber(){
+int[] arrayNum = new int[8];
+    for (int i = 0; i < arrayNum.Length; i++)
+    {
+        Console.WriteLine($"Введите числовой {i} элемент массива!");
+        arrayNum[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("Ниже список элементов массива!");
+    for (int i = 0; i < arrayNum.Length; i++)
+    {
+    Console.Write($"{arrayNum[i]}; ");
+    }
+Console.WriteLine("Hapy End!");
+}
+
+arrayEightNumber();
