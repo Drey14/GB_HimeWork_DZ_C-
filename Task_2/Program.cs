a059e65a6d4e3626f10ae87787cccf4df9d90843
”Andrey <Soda14@mail.ru>
2023-06-06 08:38:40 +0300

    Download DZ_5C#

---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+void RandomNumberArrayIndex(){
+int totalCount = 0;
+int[] array = new int[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-999,1000);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}; ");
+    if (i%2 != 0)
+    {
+        totalCount = totalCount+array[i];
+    }
+}
+    Console.WriteLine();
+    Console.WriteLine($"Cумма элементов, стоящих на нечётных индексах равна: {totalCount}");
+}
+
+RandomNumberArrayIndex();
