8728aa20e3c9a04437455b0ac756247cb54e42be
”Andrey <Soda14@mail.ru>
2023-06-23 23:24:53 +0300

    Update Task

---
+++ Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int CalculationOfTheAckermannFunction (int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return CalculationOfTheAckermannFunction(n - 1, 1);
+    else
+      return CalculationOfTheAckermannFunction(n - 1, CalculationOfTheAckermannFunction(n, m - 1));
+}
+Console.WriteLine("Задайте значение N.");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение M.");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"значение функции Аккермана равно: {CalculationOfTheAckermannFunction(num1,num2)}");
