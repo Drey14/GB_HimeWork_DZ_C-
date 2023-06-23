8696f66aae254a186336a637fa9e7cf2e400e746
”Andrey <Soda14@mail.ru>
2023-06-23 19:11:38 +0300

    Update Task2

---
+++ Task_3/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+Console.Clear();
+    Console.WriteLine("Введите число - m1 строк первого двумерного массива!");
+    int m1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число - n1 столбцов первого двумерного массива!");
+    int n1  = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число - m1 строк первого двумерного массива!");
+    int m2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число - n1 столбцов первого двумерного массива!");
+    int n2  = Convert.ToInt32(Console.ReadLine());
+    int[,] twoDimensionalArrayOne = new int[m1,n1];
+    int[,] twoDimensionalArrayTwo = new int[m2,n2];
+
+
+    void FillingTwoDimensionalArrayWithRandomNumbers(int[,] twoDimensionalArray){
+    
+        for (int i = 0; i < twoDimensionalArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < twoDimensionalArray.GetLength(1); j++)
+            {
+                twoDimensionalArray[i,j] = new Random().Next(1,4);
+            }
+        }
+    
+    }
+    
+    void ShowTwoArrayValues(int[,] twoDimensionalArray){
+    Console.WriteLine("**************************************");
+
+          for (int i = 0; i < twoDimensionalArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < twoDimensionalArray.GetLength(1); j++)
+            {
+                 Console.Write($"{twoDimensionalArray[i,j]}, ");
+            }
+            Console.WriteLine(String.Empty);
+        }
+    Console.WriteLine("**************************************");
+    }
+
+    int[,] CompatibilytyCheckMatrix(){
+        if (n1 != m1)
+        {
+            Console.WriteLine("Матрицы могут быть перемножены, если они совместимы в том смысле, что число столбцов первой матрицы равно числу строк второй ");
+            Console.WriteLine("Условия равества у ВАс при создании матриц НЕ выполнено (n1 != m1). ");
+            return null;
+        }else
+            return MultipalNewMatrix();
+    }
+
+    int[,] MultipalNewMatrix(){
+      int m3 = 0, n3 = 0;
+    
+        if (m1>m2)
+        {
+          m3=m1;
+        }else 
+          m3=m2;
+
+        if (n1>n2)
+        {
+          n3=n1;
+        }else 
+          n3=n2;
+    return new int[m3,n3];
+    } 
+
+
+    int[,] MultiplicationOfTwoMatrices(int[,] matrix1, int[,] matrix2){
+        int[,] newMultMatrix = CompatibilytyCheckMatrix();
+        int sumatorElement = 0;
+
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {   
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix2.GetLength(0); k++)
+                {
+                    sumatorElement = sumatorElement+(matrix1[i,k]*matrix2[k,j]);
+                }
+                newMultMatrix[i,j] = sumatorElement;
+                sumatorElement=0;
+            }
+        }
+    
+        return newMultMatrix;
+    }
+
+    FillingTwoDimensionalArrayWithRandomNumbers(twoDimensionalArrayOne);
+    FillingTwoDimensionalArrayWithRandomNumbers(twoDimensionalArrayTwo);
+    ShowTwoArrayValues(twoDimensionalArrayOne);
+    ShowTwoArrayValues(twoDimensionalArrayTwo);
+    ShowTwoArrayValues(MultiplicationOfTwoMatrices(twoDimensionalArrayOne,twoDimensionalArrayTwo));
+
---
+++ Task_4/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+    void FillingThreeDimensionalArrayWithRandomNumbers(int[,,] threeDimensionalArray){
+    
+        for (int i = 0; i < threeDimensionalArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < threeDimensionalArray.GetLength(1); j++)
+            {
+                for (int k = 0; k < threeDimensionalArray.GetLength(2); k++)
+                {
+                  threeDimensionalArray[i,j,k] = new Random().Next(10,100);
+                }
+            }
+        }
+    
+    }
+    
+        void ShowTwoArrayValues(int[,,] threeDimensionalArray){
+    Console.WriteLine("**************************************");
+
+          for (int i = 0; i < threeDimensionalArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < threeDimensionalArray.GetLength(1); j++)
+            {
+                for (int k = 0; k < threeDimensionalArray.GetLength(2); k++)
+                {
+                Console.Write($" значение: {threeDimensionalArray[i,j,k]}, индексы: i={i} j={j} k={k}  ");
+                }
+            }
+            Console.WriteLine(String.Empty);
+        }
+    Console.WriteLine("**************************************");
+    }
+
+int[,,] threeDimensionalArray = new int[2,2,2];
+
+FillingThreeDimensionalArrayWithRandomNumbers(threeDimensionalArray);
+ShowTwoArrayValues(threeDimensionalArray);
